46ba1c691cc35b9b51e50eb868d67a0ac8720db1
broadsign-lekevoid <kevin.gagnon@broadsign.com>
2023-04-02 23:27:34 -0400

    Initial Commit

---
+++ src/boot/supabase.js
@@ -0,0 +1,16 @@
+import { createClient } from "@supabase/supabase-js";
+import useAuthUser from "src/composables/UseAuthUser";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+supabase.auth.onAuthStateChange((evt, session) => {
+	const { user } = useAuthUser();
+	user.value = session?.user || null;
+});
+
+export default function useSupabase() {
+	return { supabase };
+}
